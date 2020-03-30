b7f8255e274c9faa8e7757d89084ce00137da3b9
Kevin <kev_sala@outlook.com>
2020-03-30 21:23:57 +0000

    Rotation sensor gathered

--- faceappwatch/src/main/java/com/example/faceappwatch/MainActivity.java
+++ faceappwatch/src/main/java/com/example/faceappwatch/MainActivity.java
@@ -1,21 +1,67 @@
 package com.example.faceappwatch;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends WearableActivity implements SensorEventListener {
 
     private TextView mTextView;
+    private SensorManager sensorManager;
+    private Sensor mLight;
+    Vibrator v;
+    private Sensor screenOrientation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        // Enables Always-on
-        setAmbientEnabled();
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        if(z > 10 || x>10 || y>10 ){
+            Log.d("Datos", Arrays.toString(event.values));
+            v.vibrate(500);
+        }
+        // Do something with this sensor value.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
